3a84450a2f79284e4a600d76833ef0c14172880e
shachar <zivshahar01@gmail.com>
2021-11-04 13:49:10 +0200

    hw q2 solution

--- src/Q2.java
+++ src/Q2.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Q2 {
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int round1=0, round2=0, round3=0;
+        int series = 0;
+        double avg = 0;
+        Scanner s =new Scanner(System.in);
+        System.out.println("enter the 1st throw:");
+        round1 = s.nextInt();
+        System.out.println("enter the 2nd throw:");
+        round2 = s.nextInt();
+        System.out.println("enter the 3rd throw:");
+        round3 = s.nextInt();
+
+        series = round1 + round2 + round3;
+        System.out.println("the sum of  all throws is:"+series);
+
+        avg = (double)series / 3;
+        System.out.println("the average is:"+avg);
+
+    }
 }
